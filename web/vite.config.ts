15fa44eebf290066b7d24c55ec9bab44c2ddd45e
danielz0102 <danyzglez@hotmail.com>
2025-08-31 18:47:45 -0600

    chore: merge vite and vitest config files

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,27 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			'~ui': '/src/components/ui',
-			'~components': '/src/components',
-			'~assets': '/src/assets',
-			'~': '/src',
+export default mergeConfig(
+	defineConfig({
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'~ui': '/src/components/ui',
+				'~components': '/src/components',
+				'~assets': '/src/assets',
+				'~': '/src',
+			},
 		},
-	},
-})
+	}),
+	defineVitestConfig({
+		test: {
+			globals: true,
+			environment: 'happy-dom',
+			setupFiles: './tests/setup.ts',
+			css: true,
+		},
+	})
+)
