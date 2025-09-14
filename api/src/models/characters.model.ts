e75647e4e40ef4a711093c483d43c420c6bd5871
danielz0102 <danyzglez@hotmail.com>
2025-09-13 20:44:04 -0600

    style(api/models): reorder and remove exports

--- api/src/models/characters.model.ts
+++ api/src/models/characters.model.ts
@@ -2,19 +2,26 @@ import { and, eq, gte, lte } from 'drizzle-orm'
 import db from '~/db'
 import { characters } from '~/db/schema'
 
-export function getAll() {
+export const CharactersModel = {
+	getAll,
+	get,
+	getAllFromScenario,
+	hasBeenClicked,
+}
+
+function getAll() {
 	return db.select().from(characters)
 }
 
-export function get(id: string) {
+function get(id: string) {
 	return db.select().from(characters).where(eq(characters.id, id))
 }
 
-export function getAllFromScenario(id: string) {
+function getAllFromScenario(id: string) {
 	return db.select().from(characters).where(eq(characters.scenarioId, id))
 }
 
-export function hasBeenClicked(id: string, { x, y }: { x: number; y: number }) {
+function hasBeenClicked(id: string, { x, y }: { x: number; y: number }) {
 	return db
 		.select()
 		.from(characters)
@@ -27,11 +34,4 @@ export function hasBeenClicked(id: string, { x, y }: { x: number; y: number }) {
 				lte(characters.maxY, y)
 			)
 		)
-}
-
-export default {
-	getAll,
-	get,
-	getAllFromScenario,
-	hasBeenClicked,
 }
--- api/src/models/scenarios.model.ts
+++ api/src/models/scenarios.model.ts
@@ -2,15 +2,15 @@ import { eq } from 'drizzle-orm'
 import db from '~/db'
 import { scenarios } from '~/db/schema'
 
+export const ScenariosModel = {
+	getAll,
+	get,
+}
+
 function getAll() {
 	return db.select().from(scenarios)
 }
 
 function get(id: string) {
 	return db.select().from(scenarios).where(eq(scenarios.id, id))
-}
-
-export default {
-	getAll,
-	get,
 }
