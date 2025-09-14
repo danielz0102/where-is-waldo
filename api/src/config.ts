f7cffca08c2ee1b323a17dd429dcc07711db1896
danielz0102 <danyzglez@hotmail.com>
2025-09-13 20:59:04 -0600

    refactor(api/config): get dbUrl depending on environment

--- api/src/config.ts
+++ api/src/config.ts
@@ -6,15 +6,22 @@ dotenv.config()
 const envSchema = z
 	.enum(['development', 'production', 'testing'])
 	.default('development')
-
 const configSchema = z.object({
 	port: z.coerce.number().default(3000),
 	nodeEnv: envSchema,
 	dbUrl: z.url(),
 })
+const nodeEnv = envSchema.parse(process.env.NODE_ENV)
+
+function getDbUrl(env: z.infer<typeof envSchema>) {
+	if (env === 'development') return process.env.DEV_DB_URL
+	if (env === 'testing') return process.env.TEST_DB_URL
+
+	return process.env.PROD_DB_URL
+}
 
 export default configSchema.parse({
 	port: process.env.PORT,
-	nodeEnv: process.env.NODE_ENV,
-	dbUrl: process.env.DATABASE_URL,
+	nodeEnv,
+	dbUrl: getDbUrl(nodeEnv),
 })
