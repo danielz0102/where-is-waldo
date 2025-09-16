600a34f28d0197055161c0abae7818f3a276602a
danielz0102 <danyzglez@hotmail.com>
2025-09-16 00:58:44 -0600

    test(api/routes/scenarios): mock models in each test and rename descriptions

--- api/tests/routes/scenarios.test.ts
+++ api/tests/routes/scenarios.test.ts
@@ -4,32 +4,30 @@ import scenariosCollection from '~tests/mocks/scenariosCollection'
 
 vi.mock('~models/ScenariosModel')
 
-beforeAll(async () => {
-	const { ScenariosModel } = await import('~models/ScenariosModel')
-	vi.mocked(ScenariosModel.getAll).mockResolvedValue(scenariosCollection)
-	vi.mocked(ScenariosModel.get).mockResolvedValue(scenariosCollection[0])
-})
-
-describe('getAll', () => {
+describe('GET /api/scenarios', () => {
 	it('responds with all scenarios', async () => {
+		const { ScenariosModel } = await import('~models/ScenariosModel')
+		vi.mocked(ScenariosModel.getAll).mockResolvedValueOnce(scenariosCollection)
+
 		const response = await request(app).get('/api/scenarios').expect(200)
 
 		expect(response.body).toEqual(scenariosCollection)
 	})
 })
 
-describe('get', () => {
-	it('responds with the identified scenario', async () => {
-		const scenario = scenariosCollection.find((s) => s.id === '1')
+describe('GET /api/scenarios/:id', () => {
+	it('responds with the scenario found', async () => {
+		const { ScenariosModel } = await import('~models/ScenariosModel')
+		vi.mocked(ScenariosModel.get).mockResolvedValueOnce(scenariosCollection[0])
 
 		const response = await request(app).get('/api/scenarios/1').expect(200)
 
-		expect(response.body).toEqual(scenario)
+		expect(response.body).toEqual(scenariosCollection[0])
 	})
 
 	it('responds with an error if the scenario does not exist', async () => {
 		const { ScenariosModel } = await import('~models/ScenariosModel')
-		vi.mocked(ScenariosModel.get).mockResolvedValueOnce(undefined)
+		vi.mocked(ScenariosModel.get).mockResolvedValueOnce(null)
 
 		const response = await request(app)
 			.get('/api/scenarios/unknown')
