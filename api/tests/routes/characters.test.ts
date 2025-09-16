f1f76d35e39a82c3e9ee2ad00db0e8acc788f356
danielz0102 <danyzglez@hotmail.com>
2025-09-16 00:32:14 -0600

    test(api/routes/characters): fix mocks usage

--- api/tests/routes/characters.test.ts
+++ api/tests/routes/characters.test.ts
@@ -4,20 +4,13 @@ import charactersCollection from '~tests/mocks/charactersCollection'
 
 vi.mock('~models/CharactersModel')
 
-beforeAll(async () => {
-	const { CharactersModel } = await import('~models/CharactersModel')
-	const CharactersModelMock = vi.mocked(CharactersModel)
-
-	CharactersModelMock.getAll.mockResolvedValue(charactersCollection)
-	CharactersModelMock.getAllFromScenario.mockResolvedValue(
-		charactersCollection.filter((character) => character.scenarioId === '1')
-	)
-	CharactersModelMock.get.mockResolvedValue(charactersCollection[0])
-	CharactersModelMock.hasBeenClicked.mockResolvedValue(true)
-})
-
 describe('GET /api/characters', () => {
 	it('responds with all characters', async () => {
+		const { CharactersModel } = await import('~models/CharactersModel')
+		vi.mocked(CharactersModel.getAll).mockResolvedValueOnce(
+			charactersCollection
+		)
+
 		const response = await request(app).get('/api/characters').expect(200)
 
 		expect(response.body).toEqual(charactersCollection)
@@ -26,20 +19,22 @@ describe('GET /api/characters', () => {
 
 describe('GET /api/characters/:id', () => {
 	it('responds with the character with the given ID', async () => {
-		const id = '1'
-		const character = charactersCollection.find((c) => c.id === id)
-		const response = await request(app).get(`/api/characters/${id}`).expect(200)
+		const character = charactersCollection[0]
+		const { CharactersModel } = await import('~models/CharactersModel')
+		vi.mocked(CharactersModel.get).mockResolvedValueOnce(character)
+
+		const response = await request(app).get('/api/characters/1').expect(200)
 
 		expect(response.body).toEqual(character)
 	})
 
 	it('responds with 404 if the character does not exist', async () => {
-		const id = 'non-existent-id'
 		const { CharactersModel } = await import('~models/CharactersModel')
-		const CharactersModelMock = vi.mocked(CharactersModel)
-		CharactersModelMock.get.mockResolvedValue(null)
+		vi.mocked(CharactersModel.get).mockResolvedValueOnce(null)
 
-		const response = await request(app).get(`/api/characters/${id}`).expect(404)
+		const response = await request(app)
+			.get('/api/characters/non-existent-id')
+			.expect(404)
 
 		expect(response.body).toEqual({ error: 'Character not found' })
 	})
@@ -47,13 +42,12 @@ describe('GET /api/characters/:id', () => {
 
 describe('GET /api/characters/:id/click', () => {
 	it('responds true if the character has been clicked', async () => {
-		const id = '1'
-		const x = 75
-		const y = 150
+		const { CharactersModel } = await import('~models/CharactersModel')
+		vi.mocked(CharactersModel.hasBeenClicked).mockResolvedValueOnce(true)
 
 		const response = await request(app)
-			.get(`/api/characters/${id}/click`)
-			.query({ x, y })
+			.get('/api/characters/1/click')
+			.query({ x: 75, y: 150 })
 			.expect(200)
 
 		expect(response.body).toEqual(true)
@@ -61,16 +55,11 @@ describe('GET /api/characters/:id/click', () => {
 
 	it('responds false if the character has not been clicked', async () => {
 		const { CharactersModel } = await import('~models/CharactersModel')
-		const CharactersModelMock = vi.mocked(CharactersModel)
-		CharactersModelMock.hasBeenClicked.mockResolvedValue(false)
-
-		const id = '1'
-		const x = 10
-		const y = 20
+		vi.mocked(CharactersModel.hasBeenClicked).mockResolvedValueOnce(false)
 
 		const response = await request(app)
-			.get(`/api/characters/${id}/click`)
-			.query({ x, y })
+			.get('/api/characters/1/click')
+			.query({ x: 75, y: 150 })
 			.expect(200)
 
 		expect(response.body).toEqual(false)
