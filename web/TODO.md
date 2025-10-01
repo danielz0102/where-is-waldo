357b2a5408a3305d653536f7c56a97ac0af2010a
danielz0102 <danyzglez@hotmail.com>
2025-09-30 23:18:20 -0600

    refactor(web): rename pages files to PascalCase

---
+++ web/TODO.md
@@ -0,0 +1,9 @@
+# Features
+
+- [ ] Add loading, error, and success states to character buttons
+- [ ] Close character menu after animation
+
+# Fixes
+
+- [x] Fix character coordinates on DB
+- [ ] Remove camelCase file names
--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,8 +1,8 @@
 import '@fontsource/indie-flower'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import LandingPage from '~/pages/landing'
-import Level from '~/pages/level'
+import Landing from '~pages/Landing'
+import Level from '~pages/Level'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +11,7 @@ function App() {
 		<QueryClientProvider client={queryClient}>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<LandingPage />} />
+					<Route path="/" element={<Landing />} />
 					<Route path="/game" element={<Level name="Beach" />} />
 				</Routes>
 			</BrowserRouter>
