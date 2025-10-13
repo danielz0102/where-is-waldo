9e44bae477eb699a6a9b46776f245b476182fcbe
danielz0102 <danyzglez@hotmail.com>
2025-10-13 13:16:05 -0600

    style(web/WinnerModal): add fade-in effect

--- web/src/components/WinnerModal.tsx
+++ web/src/components/WinnerModal.tsx
@@ -7,7 +7,7 @@ export default function WinnerModal() {
 	return (
 		<dialog
 			open={win}
-			className="-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 rounded bg-gradient-to-br from-blue-100 to-red-100 px-8 py-4 text-center shadow shadow-red-200"
+			className="-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 animate-duration-150 animate-fade-in rounded bg-gradient-to-br from-blue-100 to-red-100 px-8 py-4 text-center shadow shadow-red-200 md:px-16 md:py-8"
 		>
 			<h2 className="mb-4 font-bold text-3xl">You won!</h2>
 			<p>
