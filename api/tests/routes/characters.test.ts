02719c97e56e31f13a799b2162ba1299e8657190
danielz0102 <danyzglez@hotmail.com>
2025-10-19 13:30:23 -0600

    tests(api): remove CharacterModelMock and update tests to use mocked methods directly

--- api/tests/routes/characters.test.ts
+++ api/tests/routes/characters.test.ts
@@ -1,34 +1,31 @@
 import request from 'supertest'
 import { CharacterModel } from '~models/CharacterModel'
 import { app } from '~tests/app'
+import { getRandomFrom } from '~tests/lib/getRandomFrom'
 import charactersCollection from '~tests/mocks/charactersCollection'
 
-vi.mock('~models/CharacterModel', async () => {
-	const { CharacterModelMock } = await import('~tests/mocks/CharacterModelMock')
-	return {
-		CharacterModel: CharacterModelMock,
-	}
-})
+vi.mock('~models/CharacterModel')
 
 const CharacterModelMock = vi.mocked(CharacterModel)
 
 describe('GET /api/characters', () => {
-	it('responds with all characters', async () => {
-		const charactersExpected = await CharacterModelMock.getAll()
+	it('responds with the array of characters received', async () => {
+		CharacterModelMock.getAll.mockResolvedValueOnce(charactersCollection)
 		const response = await request(app).get('/api/characters').expect(200)
-		expect(response.body).toEqual(charactersExpected)
+		expect(response.body).toEqual(charactersCollection)
 	})
 
-	it('responds with all characters from the scenario passed', async () => {
-		const { scenarioId } = getFakeCharacter()
-		const scenariosExpected =
-			await CharacterModelMock.getAllFromScenario(scenarioId)
+	it('responds with an array of characters received when a scenarioId is provided', async () => {
+		const fakeId = crypto.randomUUID()
+		CharacterModelMock.getAllFromScenario.mockImplementationOnce(async (id) => {
+			return id === fakeId ? charactersCollection : []
+		})
 
 		const response = await request(app)
-			.get(`/api/characters?scenarioId=${scenarioId}`)
+			.get(`/api/characters?scenarioId=${fakeId}`)
 			.expect(200)
 
-		expect(response.body).toEqual(scenariosExpected)
+		expect(response.body).toEqual(charactersCollection)
 	})
 
 	it('responds with status 400 if scenarioId is not a valid UUID', async () => {
@@ -40,7 +37,10 @@ describe('GET /api/characters', () => {
 
 describe('GET /api/characters/:id', () => {
 	it('responds with the character with the given ID', async () => {
-		const fakeCharacter = getFakeCharacter()
+		const fakeCharacter = getRandomFrom(charactersCollection)
+		CharacterModelMock.getById.mockImplementationOnce(async (id) => {
+			return id === fakeCharacter.id ? fakeCharacter : null
+		})
 
 		const response = await request(app)
 			.get(`/api/characters/${fakeCharacter.id}`)
@@ -50,42 +50,32 @@ describe('GET /api/characters/:id', () => {
 	})
 
 	it('responds with 404 if the character does not exist', async () => {
-		const response = await request(app)
-			.get(`/api/characters/${crypto.randomUUID()}`)
-			.expect(404)
-
-		expect(response.body).toEqual({ error: 'Character not found' })
+		CharacterModelMock.getById.mockResolvedValueOnce(null)
+		await request(app).get(`/api/characters/${crypto.randomUUID()}`).expect(404)
 	})
 
-	it('responds with 400 if the ID is not a valid UUID', async () => {
-		await request(app).get('/api/characters/invalid-uuid').expect(400)
+	it('responds with 400 if the ID is not a valid UUID', () => {
+		return request(app).get('/api/characters/invalid-uuid').expect(400)
 	})
 })
 
 describe('GET /api/characters/:id/click', () => {
-	it('responds true if the character has been clicked', async () => {
-		const fakeCharacter = getFakeCharacter()
+	beforeAll(() => {
+		CharacterModelMock.click.mockResolvedValue(true)
+	})
 
+	it('responds true if the character has been clicked', async () => {
 		const response = await request(app)
-			.get(`/api/characters/${fakeCharacter.id}/click`)
-			.query({ x: fakeCharacter.maxX, y: fakeCharacter.maxY })
+			.get(`/api/characters/${crypto.randomUUID()}/click`)
+			.query({ x: 100, y: 50 })
 			.expect(200)
 
 		expect(response.body).toEqual(true)
 	})
 
 	it('responds false if the character has not been clicked', async () => {
-		const fakeCharacter = getFakeCharacter()
+		CharacterModelMock.click.mockResolvedValueOnce(false)
 
-		const response = await request(app)
-			.get(`/api/characters/${fakeCharacter.id}/click`)
-			.query({ x: fakeCharacter.maxX + 1, y: fakeCharacter.maxY + 1 })
-			.expect(200)
-
-		expect(response.body).toEqual(false)
-	})
-
-	it('responds false if the character does not exist', async () => {
 		const response = await request(app)
 			.get(`/api/characters/${crypto.randomUUID()}/click`)
 			.query({ x: 100, y: 100 })
@@ -116,35 +106,22 @@ describe('GET /api/characters/:id/click', () => {
 	})
 
 	it('responds with 400 if coordinates are missing', async () => {
-		const fakeCharacter = getFakeCharacter()
 		await request(app)
-			.get(`/api/characters/${fakeCharacter.id}/click`)
+			.get(`/api/characters/${crypto.randomUUID()}/click`)
 			.expect(400)
 	})
 
 	it('responds with 400 if coordinates are not numbers', async () => {
-		const fakeCharacter = getFakeCharacter()
 		await request(app)
-			.get(`/api/characters/${fakeCharacter.id}/click`)
+			.get(`/api/characters/${crypto.randomUUID()}/click`)
 			.query({ x: {}, y: '; DELETE FROM characters;' })
 			.expect(400)
 	})
 
 	it('responds with 400 if coordinates are negative', async () => {
-		const fakeCharacter = getFakeCharacter()
 		await request(app)
-			.get(`/api/characters/${fakeCharacter.id}/click`)
+			.get(`/api/characters/${crypto.randomUUID()}/click`)
 			.query({ x: -10, y: -20 })
 			.expect(400)
 	})
 })
-
-function getFakeCharacter() {
-	const fakeCharacter = charactersCollection[0]
-
-	if (!fakeCharacter) {
-		throw new Error('Character with index 0 was not found in the collection')
-	}
-
-	return fakeCharacter
-}
