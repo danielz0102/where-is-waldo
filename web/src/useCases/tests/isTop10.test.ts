5ec4ad757bc29c9f86005341a1b8609d45ef2290
danielz0102 <danyzglez@hotmail.com>
2025-11-11 01:36:58 -0600

    test(web): refactor isTop10 tests to use vi.mock for ScoreService

--- web/src/useCases/tests/isTop10.test.ts
+++ web/src/useCases/tests/isTop10.test.ts
@@ -3,37 +3,41 @@ import ScoreService from '~/services/ScoreService'
 import { isTop10 } from '~/useCases/isTop10'
 import { createRandomScores } from '~tests/utils/fakeData'
 
-const getTop10Mock = vi.spyOn(ScoreService, 'getTop10ByScenario')
+vi.mock('~/services/ScoreService', () => ({
+	default: {
+		getTop10ByScenario: vi.fn(),
+	},
+}))
 
-describe('isTop10', () => {
-	it('returns true if there are less than 10 scores', async () => {
-		getTop10Mock.mockResolvedValueOnce(createRandomScores(5))
+const getTop10Mock = vi.mocked(ScoreService.getTop10ByScenario)
 
-		const result = await isTop10(crypto.randomUUID(), 60)
+test('returns true if there are less than 10 scores', async () => {
+	getTop10Mock.mockResolvedValueOnce(createRandomScores(5))
 
-		expect(result).toBe(true)
+	const result = await isTop10(crypto.randomUUID(), 60)
+
+	expect(result).toBe(true)
+})
+
+test('returns true if the new score is better than at least one of the top 10 scores', async () => {
+	const fakeScores = createRandomScores(10)
+	getTop10Mock.mockResolvedValueOnce(fakeScores)
+
+	const newScoreInSeconds = timeToSeconds(fakeScores[0].time) - 1
+	const result = await isTop10(crypto.randomUUID(), newScoreInSeconds)
+
+	expect(result).toBe(true)
+})
+
+test('returns false if the new score is worse than all of the top 10 scores', async () => {
+	const fakeScores = createRandomScores(10)
+	getTop10Mock.mockResolvedValueOnce(fakeScores)
+	const worstScore = fakeScores.reduce((prev, curr) => {
+		return timeToSeconds(prev.time) > timeToSeconds(curr.time) ? prev : curr
 	})
 
-	it('returns true if the new score is better than at least one of the top 10 scores', async () => {
-		const fakeScores = createRandomScores(10)
-		getTop10Mock.mockResolvedValueOnce(fakeScores)
+	const newScore = timeToSeconds(worstScore.time) + 1
+	const result = await isTop10(crypto.randomUUID(), newScore)
 
-		const newScoreInSeconds = timeToSeconds(fakeScores[0].time) - 1
-		const result = await isTop10(crypto.randomUUID(), newScoreInSeconds)
-
-		expect(result).toBe(true)
-	})
-
-	it('returns false if the new score is worse than all of the top 10 scores', async () => {
-		const fakeScores = createRandomScores(10)
-		getTop10Mock.mockResolvedValueOnce(fakeScores)
-		const worstScore = fakeScores.reduce((prev, curr) => {
-			return timeToSeconds(prev.time) > timeToSeconds(curr.time) ? prev : curr
-		})
-
-		const newScore = timeToSeconds(worstScore.time) + 1
-		const result = await isTop10(crypto.randomUUID(), newScore)
-
-		expect(result).toBe(false)
-	})
+	expect(result).toBe(false)
 })
